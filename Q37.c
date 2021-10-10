56c540d86e988799ad262d83ba83fd09adc2056b
Amansrivastava1312 <91677006+Amansrivastava1312@users.noreply.github.com>
2021-10-10 07:08:03 -0700

    Add files via upload

---
+++ Q37.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ 
+float num = 123678.567;
+ // %e is format specifier for exponential form//
+
+    printf("value in exponential form is %e",num);
+
+    return 0;
+}
